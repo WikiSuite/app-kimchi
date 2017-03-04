183258cecf26819de5771fce2f83836c55a46880
Marc Laporte <marc@laporte.name>
2017-03-04 08:37:23 -0500

    Improving description for less technical users

--- language/en_US/kimchi_lang.php
+++ language/en_US/kimchi_lang.php
@@ -1,7 +1,7 @@
 <?php
 
 $lang['kimchi_app_name'] = 'Kimchi';
-$lang['kimchi_app_description'] = 'Kimchi is an HTML5 based management tool for KVM. It is designed to make it as easy as possible to get started with KVM.';
+$lang['kimchi_app_description'] = 'Kimchi is an HTML5 based management tool for KVM. It is designed to make it as easy as possible to get started with KVM virtualization.';
 $lang['kimchi_admin_console'] = 'Kimchi Admin Console';
 $lang['kimchi_admin_console_help'] = 'Follow the link to access the Kimchi Admin Console.';
 $lang['kimchi_go_to_admin_console'] = 'Go To Admin Console';
